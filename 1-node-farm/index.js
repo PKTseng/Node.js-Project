e1da3ff7b3de58509dd2c813fe120cfcbbdaf9f4
PKTseng <jk0910948659@Gmail.com>
2021-01-24 16:27:43 +0800

    add readFileSync

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -6,10 +6,19 @@ const fs = require("fs");
 // console.log(hello);
 
 // 執行這個檔案
-const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// console.log(textIn);
 
 // 新增檔案
-const textOut = `this is a text file : ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./starter/txt/output.txt", textOut);
-console.log("File written !!");
+// const textOut = `this is a text file : ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./starter/txt/output.txt", textOut);
+// console.log("File written !!");
+
+// callback hell
+fs.readFileSync("./starter/txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!!");
+  console.log(data1);
+  fs.readFileSync("./starter/txt/append.txt", "utf-8", (err, data2) => {
+    console.log(data2);
+  });
+});
